cc5473da514b1cc64ae1e1f7846a98bb94dcba77
Razhabka <orlov.22801@gmail.com>
2025-01-13 11:45:36 +0300

    Update

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -3,9 +3,9 @@ public class Run {
     public static void main(String[] args) {
         String[][] array = new String[][]
                 {{"4", "7", "100", "24"},
-                {"54", "73", "46", "52"},
-                {"9", "63", "23", "73"},
-                {"2", "28", "44", "23"}};
+                        {"54", "73", "46", "52"},
+                        {"9", "63", "23", "73"},
+                        {"2", "28", "44", "23"}};
         try {
             System.out.println(arraySum(array));
         } catch (MyArraySizeException | MyArrayDataException e) {
@@ -15,11 +15,12 @@ public class Run {
     }
 
     public static int arraySum(String[][] arr) throws MyArraySizeException, MyArrayDataException {
-        if(arr.length != 4){
+        if (arr.length != 4) {
             throw new MyArraySizeException("Неверная длинна массива");
-        }else {
-            for(int i = 0; i < arr.length; i++){
-                if(arr[i].length != 4) throw new MyArraySizeException("Неверная длина вутренненго масива под индексом " + i);
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].length != 4)
+                    throw new MyArraySizeException("Неверная длина вутренненго масива под индексом " + i);
             }
         }
 
