aad22a459487d8c3271d282b0bcebe84407e99f6
Razhabka <orlov.22801@gmail.com>
2025-01-16 10:29:34 +0300

    Update

--- src/main/java/MyArrayDataException.java
+++ src/main/java/MyArrayDataException.java
@@ -1,6 +1,6 @@
-class MyArraySizeException extends Exception {
-
-    public MyArraySizeException(String message) {
-        super(message);
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message, int index1, int index2) {
+        super(message + " ячейках массивов под индексами " + index1 + " " + index2);
     }
+
 }
--- src/main/java/MyArraySizeException.java
+++ src/main/java/MyArraySizeException.java
@@ -1,6 +1,6 @@
-class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message, int index1, int index2) {
-        super(message + " ячейках массивов под индексами " + index1 + " " + index2);
-    }
+class MyArraySizeException extends Exception {
 
+    public MyArraySizeException(String message) {
+        super(message);
+    }
 }
