6ac29156420f290889e0cd574fad26c7bf239c81
Razhabka <orlov.22801@gmail.com>
2025-01-21 13:30:07 +0300

    Update after the first check

--- src/main/java/ProgramForTesting.java
+++ src/main/java/ProgramForTesting.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 
 public class ProgramForTesting {
     public static BigInteger factorial(BigInteger num) throws WrongArgumentException {
-        if (num.longValue() == 1 || num.longValue() == 0) {
+        if (num.equals(BigInteger.ONE) || num.equals(BigInteger.ZERO)) {
             return BigInteger.valueOf(1);
         } else if (num.longValue() < 0) {
             throw new WrongArgumentException("Передан неверный аргумент");
--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -3,7 +3,7 @@ import java.math.BigInteger;
 public class Run {
     public static void main(String[] args) {
         try {
-            System.out.println(ProgramForTesting.factorial(new BigInteger("20")));
+            System.out.println(ProgramForTesting.factorial(new BigInteger("1")));
         } catch (WrongArgumentException e) {
             e.printStackTrace();
         }
