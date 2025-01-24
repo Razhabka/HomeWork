4962401cda5ae5ab684dece615ce46d44d0136f9
Razhabka <orlov.22801@gmail.com>
2025-01-24 14:59:47 +0300

    Upload

--- src/test/java/by/mts/mtsTest/TestMTS.java
+++ src/test/java/by/mts/mtsTest/TestMTS.java
@@ -13,5 +13,42 @@ public class TestMTS extends SetUpDriver {
 
     private final MainPage mainPage = new MainPage();
 
+    @Test
+    @Description("Проверка надписи полей 'Услуга связи'")
+    public void comServiceTest(){
+        Assertions.assertAll(
+                () -> Assertions.assertEquals("Номер телефона", mainPage.communSeviceText()[0]),
+                () -> Assertions.assertEquals("Сумма", mainPage.communSeviceText()[1]),
+                () -> Assertions.assertEquals("E-mail для отправки чека", mainPage.communSeviceText()[2])
+        );
+    }
+    @Test
+    @Description("Проверка надписи полей'Домашний интернет'")
+    public void homeInternetTest(){
+        Assertions.assertAll(
+                ()->Assertions.assertEquals("Номер абонента", mainPage.homeNetText()[0]),
+                ()->Assertions.assertEquals("Сумма", mainPage.homeNetText()[1]),
+                ()->Assertions.assertEquals("E-mail для отправки чека", mainPage.homeNetText()[2])
+        );
+    }
 
+    @Test
+    @Description("Проверка надписей полей 'Рассрочка'")
+    public void installmentTest(){
+        Assertions.assertAll(
+                ()->Assertions.assertEquals("Номер счета на 44", mainPage.installmentText()[0]),
+                ()->Assertions.assertEquals("Сумма", mainPage.installmentText()[1]),
+                ()->Assertions.assertEquals("E-mail для отправки чека", mainPage.installmentText()[2])
+        );
+    }
+
+    @Test
+    @Description("Проверка надписей полей 'Рассрочка'")
+    public void arrearsTest(){
+        Assertions.assertAll(
+                ()->Assertions.assertEquals("Номер счета на 2073", mainPage.arrearsText()[0]),
+                ()->Assertions.assertEquals("Сумма", mainPage.arrearsText()[1]),
+                ()->Assertions.assertEquals("E-mail для отправки чека", mainPage.arrearsText()[2])
+        );
+    }
 }
--- src/test/java/by/mts/pages/MainPage.java
+++ src/test/java/by/mts/pages/MainPage.java
@@ -10,17 +10,56 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class MainPage extends BasePage {
-    @FindBy(xpath = "//div[@class='pay__wrapper']/h2")
-    private WebElement payWrapper;
-
     @FindBy(id = "connection-phone")
     private WebElement phoneNumberField;
 
     @FindBy(id = "connection-sum")
     private WebElement sumField;
 
+    @FindBy(id = "connection-email")
+    private WebElement emailField;
+
+    @FindBy(id = "internet-phone")
+    private WebElement internetPhone;
+
+    @FindBy(id = "internet-sum")
+    private WebElement internetSum;
+
+    @FindBy(id = "internet-email")
+    private WebElement internetEmail;
+
+    @FindBy(id = "score-instalment")
+    private WebElement instalmentScore;
+
+    @FindBy(id = "instalment-sum")
+    private WebElement instalmentSum;
+
+    @FindBy(id = "instalment-email")
+    private WebElement instalmentEmail;
+
+    @FindBy(id = "score-arrears")
+    private WebElement arrearsScore;
+
+    @FindBy(id = "arrears-sum")
+    private WebElement arrearsSum;
+
+    @FindBy(id = "arrears-email")
+    private WebElement arrearsEmail;
+
     @FindBy(xpath = "//form[1]/button[@class='button button__default ']")
     private WebElement continueBtn;
+
+    @FindBy(xpath = "//button[@class='select__header']")
+    private WebElement selectHeaderBtn;
+
+    @FindBy(xpath = "//ul[@class='select__list']/li[2]")
+    private WebElement homeInternet;
+
+    @FindBy(xpath = "//ul[@class='select__list']/li[3]")
+    private WebElement installmentBtn;
+
+    @FindBy(xpath = "//ul[@class='select__list']/li[4]")
+    private WebElement arrearsBtn;
 
     @FindBy(xpath = "//div[@class='pay-description__text']/span")
     private WebElement payDescriptionNumber;
@@ -35,32 +74,55 @@ public class MainPage extends BasePage {
         acceptCookies();
     }
 
-
-    public String replenishment(String phoneNumber, String sum) {
-
-        phoneNumberField.click();
-        phoneNumberField.sendKeys(phoneNumber);
-        sumField.click();
-        sumField.sendKeys(sum);
-        continueBtn.click();
-        driver.switchTo().frame(driver.findElement(new By.ByXPath("//iframe[@class='bepaid-iframe']")));
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(new By.ByXPath("//div[@class='pay-description__text']/span")));
-        return getSumAndNumberPhone(payDescriptionSum.getText(), payDescriptionNumber.getText());
+    public String[] communSeviceText(){
+        String[] allPlaceHolders = new String[3];
+        allPlaceHolders[0] = phoneNumberField.getAttribute("placeholder");
+        allPlaceHolders[1] = sumField.getAttribute("placeholder");
+        allPlaceHolders[2] = emailField.getAttribute("placeholder");
+        return allPlaceHolders;
     }
 
-    private String getSumAndNumberPhone(String sum, String numberPhone) {
-        String[] splitSum = sum.split("\\.");
-        String[] splitNumber = numberPhone.split(":");
-        return splitSum[0] + splitNumber[2];
+    public String[] homeNetText(){
+        selectHeaderBtn.click();
+//        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//li[2][@class='select__item active']")));
+        homeInternet.click();
+//        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(By.id("internet-phone")));
+        String[] allPlaceHolders = new String[3];
+        allPlaceHolders[0] = internetPhone.getAttribute("placeholder");
+        allPlaceHolders[1] = internetSum.getAttribute("placeholder");
+        allPlaceHolders[2] = internetEmail.getAttribute("placeholder");
+        return allPlaceHolders;
+
     }
+
+    public String[] installmentText(){
+        selectHeaderBtn.click();
+        installmentBtn.click();
+
+        String[] allPlaceHolders = new String[3];
+        allPlaceHolders[0] = instalmentScore.getAttribute("placeholder");
+        allPlaceHolders[1] = instalmentSum.getAttribute("placeholder");
+        allPlaceHolders[2] = instalmentEmail.getAttribute("placeholder");
+        return allPlaceHolders;
+    }
+
+    public String[] arrearsText(){
+        selectHeaderBtn.click();
+        arrearsBtn.click();
+
+        String[] allPlaceHolders = new String[3];
+        allPlaceHolders[0] = arrearsScore.getAttribute("placeholder");
+        allPlaceHolders[1] = arrearsSum.getAttribute("placeholder");
+        allPlaceHolders[2] = arrearsEmail.getAttribute("placeholder");
+        return allPlaceHolders;
+    }
+
 
     private static void acceptCookies() {
-
         try {
             WebElement webElement = driver.findElement(By.id("cookie-agree"));
             webElement.click();
         } catch (NoSuchElementException | ElementNotInteractableException e) {
-
         }
     }
 }
